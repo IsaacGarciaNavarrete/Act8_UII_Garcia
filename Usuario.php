45ef7f23449d88ebe57076b5130abb68fc6b988a
IsaacGarciaNavarrete <91344700+IsaacGarciaNavarrete@users.noreply.github.com>
2021-11-02 10:57:45 -0600

    Add files via upload

---
+++ Usuario.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+<body>
+<link rel="stylesheet" href="style.css">
+<h1 class="Titulo">Iniciar sesion</h1>
+
+<form action="/action_page.php">
+  <label for="fname">Nombre de usuario:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Correo Electronico:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <label for="fname">Contraseña:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Enviar">
+</form>
+
+<p>Presiona el botton cuando hayas terminado</p>
+</form>	
+
+</body>
+</html>
---
+++ empleado.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+<link rel="stylesheet" href="style.css">
+<h1 class="Titulo">Formulario de Registro</h1>
+
+<form action="/action_page.php">
+  <label for="fname">Nombre:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Apellidos:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <label for="fname">Edad:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="fname">INE:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="fname">CURP:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="fname">Certificado:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <input type="submit" value="Enviar">
+</form>
+
+<p>Presiona el botton cuando hayas terminado</p>
+</form>	
+
+</body>
+</html>
