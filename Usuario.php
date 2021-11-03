108c337637bbee6f2d51d3c46a8e18f58050df64
IsaacGarciaNavarrete <91344700+IsaacGarciaNavarrete@users.noreply.github.com>
2021-11-03 13:29:20 -0600

    Update and rename Usuario.html to Usuario.php

--- Usuario.php
+++ Usuario.php
@@ -1,6 +1,28 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+    <title>13 Isaac Garcia Navarrete</title>
+    
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="https://mir-s3-cdn-cf.behance.net/projects/404/144b73108150787.Y3JvcCw4NDAsNjU3LDI3OCwyNzA.jpg" alt="IZZI"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="inicio.php">Inicio</a></li>
+					<li><a href="empleado.php">Empleado</a></li>
+					<li><a href="Usuario.php">Usuario</a></li>
+					<li><a href="Ventas.php">Ventas</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+    </head>
         
     </head>
 <body>
